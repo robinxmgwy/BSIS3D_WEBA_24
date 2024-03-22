8cedd3eb3cd41fd6d82643cef6bf20ce6bbab378
reeennnn <renirubio000@gmail.com>
2024-03-22 12:37:56 +0800

    com

--- mvc/app/core/Model.php
+++ mvc/app/core/Model.php
@@ -24,6 +24,61 @@ public function findALL()
 }
 public function where($data, $data_not = [])
  {
-    
+    $keys = array_keys($data);
+    $keys_not = array_keys($data_not);
+
+    $query = "select * from $this->table where";
+
+    foreach (keys as $key) {
+        $query .= $key .  != : . $key . " && ";
+    }
+
+    $query = trim($query. ' && ');
+
+    $data = array_merge($data, $data_not);
+    $result = $this->query($query, $data);
+
+    if (result) {
+        return $result:
+    }
+    return false;
  }
+
+ public function insert($data)
+ {
+    $columns = implode (',', array_keys($data));
+    $values = implode (',', array_keys($data));
+    $query = "insert into $this->table ($columns) values (:$values)";
+    show($query);
+    $this->query($query, $data);
+
+    return false;
+ }
+ public function updated($id, $data, $column = 'id')
+ {
+    $keys = array_keys($data);
+    $query = "update $this->table set";
+
+    foreach ($keys as $key) {
+        $query .= $key . " = " . keys . ",";
+    }
+
+    $query = trim($query, " , ");
+
+    $query .= " where $column =  :$column";
+
+    $data[$column] = $id;
+    $this->query($query, $data);
+ }
+ return false;
+}
+
+public function delete($id, $column = 'id')
+{
+    $data{$column} = $id;
+    $query = "delete from $this->table where $column = :$column";
+
+    $this->query($query, $data);
+
+    return false;
 }
--- mvc/app/core/init.php
+++ mvc/app/core/init.php
@@ -1,4 +1,12 @@
 <?php 
 
 require 'functions.php';
+require 'Database.php';
+require 'Controller.php';
+require 'Model.php';
 require 'App.php';
+
+spl_autoload_register(function ($classname) {
+
+    require '../app/models/' . $class_name . '.php';
+});
